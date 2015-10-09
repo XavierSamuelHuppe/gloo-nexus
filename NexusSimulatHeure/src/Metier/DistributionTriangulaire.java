1854cff1e89344f4bdad6e7a6f29125bcbed019a
XavierSamuelHuppe <xhuppe@gmail.com>
2015-10-08 20:01:04 -0400

    Ajout de la distribution triangulaire

---
+++ NexusSimulatHeure/src/Metier/DistributionTriangulaire.java
@@ -0,0 +1,26 @@
+package Metier;
+
+public class DistributionTriangulaire {
+    
+    private double a,b,c;
+    
+    public DistributionTriangulaire(double min, double mode, double max){
+        this.a = min;
+        this.b = max;
+        this.c = mode;
+    }
+    
+    public double obtenirProchaineValeurAleatoire(){
+        double retour;
+        double x = Math.random();
+        if (x == 0) {
+            retour = a;
+        } else if (x < (c - a) / (b - a)) {
+            retour = a + Math.sqrt(x * (b - a) * (c - a));
+        } else {
+            retour = b - Math.sqrt((1 - x) * (b - a) * (b - c));
+        }
+        return retour;
+        //Credit: https://en.wikipedia.org/wiki/Triangular_distribution
+    }
+}
