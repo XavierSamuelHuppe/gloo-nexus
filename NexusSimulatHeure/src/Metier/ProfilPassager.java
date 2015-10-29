36c76a1a4304e4a7f290bf9d55a9db9ce0d0b305
Pierre Pelletier <pierrepelletier64@gmail.com>
2015-10-28 21:59:07 -0400

    Code de ProfilPassager

---
+++ NexusSimulatHeure/src/Metier/ProfilPassager.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Metier;
+
+/**
+ *
+ * @author Utilisateur
+ */
+public class ProfilPassager {
+    private Point pointDepart;
+    private int heureDepart;
+    private int frequence;
+    /* existe pas encore
+    private Distribution distributionAUtiliser;
+    */
+    private Statistiques statistiques;
+    
+    private void pigerDonneesDepart()
+    {
+        //jajajajajaja
+    }/*  décommenter quand on va travailler la dessus, je sais pas trop quel genre de Mouseevent il veut. 
+    public Passager genererPassager()
+    {
+        Passager unPieton = new Passager();
+        return unPieton;
+    }
+    */
+}
