5c55218318826ae2dae2e8a1e581d35e955e490b
XavierSamuelHuppe <xhuppe@gmail.com>
2015-10-28 20:24:53 -0400

    Ajout des classes Passager (STUB) et ConteneurPassagers (interface)

---
+++ NexusSimulatHeure/src/Metier/ConteneurPassagers.java
@@ -0,0 +1,7 @@
+package Metier;
+
+public interface ConteneurPassagers {
+    public int obtenirNombrePassagers();
+    public void ajouterPassager(Passager passager);
+    public void retirerPassager(Passager passager);
+}
