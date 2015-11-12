cab2995d615541c539d1d2b84af24b310aef810e
Pierre Pelletier <pierrepelletier64@gmail.com>
2015-11-12 14:45:30 -0500

    Enlever les commentaires

--- NexusSimulatHeure/src/Metier/Source/SourceHeureFin.java
+++ NexusSimulatHeure/src/Metier/Source/SourceHeureFin.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Metier.Source;
 
 import Metier.Carte.Point;
 import Metier.DistributionTriangulaire;
 
-/**
- *
- * @author Utilisateur
- */
 public class SourceHeureFin extends Source {
     private int heureFin;
     
