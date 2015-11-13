a263f7e08306cfef06e5fd2c3ef7658336d7ffb5
Pierre Pelletier <pierrepelletier64@gmail.com>
2015-11-13 16:28:30 -0500

    Ajout des nouvelle méthode pour ajouter/retirer une Source. Retrait des anciennes + ceux de profilPassager.

--- NexusSimulatHeure/src/Metier/Simulation/Simulation.java
+++ NexusSimulatHeure/src/Metier/Simulation/Simulation.java
@@ -82,87 +82,18 @@ public class Simulation{
         circuits.remove(circuit);
     }
     
-    public void modifierSourceHeureFin(Metier.Source.SourceHeureFin sourceHeureFin, double heureFin ,Point pointDepart, double heureDebut, double frequence){
-        /*
-        int index = profils.indexOf(sourceHeureFin);
-        SourceHeureFin sourceAChanger = sources.get(index);
-        
-        sourceAChanger.setFrequence(frequence);
-        sourceAChanger.setheureDebut(heureDebut);
-        sourceAChanger.setPointDepart(pointDepart);
-        sourceAChanger.setheureFin(heureFin);
-        */
-    }
-    public void modifierSourceFinie(Metier.Source.SourceFinie sourceFinie, int nombreMax ,Point pointDepart, double heureDebut, double frequence){
-        /*
-        int index = profils.indexOf(sourceFinie);
-        SourceFinie sourceAChanger = sources.get(index);
-        
-        sourceAChanger.setFrequence(frequence);
-        sourceAChanger.setheureDebut(heureDebut);
-        sourceAChanger.setPointDepart(pointDepart);
-        sourceAChanger.setNombreMax(nombreMax);*/
+    public void ajouterSource(Source source){
+        sources.add(source);
     }
     
-    public void modifierProfilPassagerFini(Metier.Profil.ProfilPassagerFini profilPassagerFini, int nombreMax, Point point, double heureDepart, double frequence, DistributionTriangulaire distribution){
-        /*
-        int index = profils.indexOf(profilPassagerFini);
-        ProfilPassagerFini profilAChanger = profils.get(index);
-        
-        profilAChanger.setFrequence(frequence);
-        profilAChanger.setHeureDepart(heureDepart);
-        profilAChanger.setPointDepart(point);
-        profilAChanger.setNombreMax(nombreMax);
-        */
+    public void retirerSource(Source source){
+        sources.remove(source);
     }
     
-    public void modifierProfilPassagerHeureFin(Metier.Profil.ProfilPassagerHeureFin profilPassagerHeureFin, double heureFin, Point point, double heureDepart, double frequence, DistributionTriangulaire distribution){
-        /*
-        int index = profils.indexOf(profilPassagerHeureFin);
-        ProfilPassagerHeureFin profilAChanger = profils.get(index);
-        
-        profilAChanger.setFrequence(frequence);
-        profilAChanger.setHeureDepart(heureDepart);
-        profilAChanger.setPointDepart(point);
-        profilAChanger.setHeureFin(heureFin);*/
-        
+    public void modifierProfilPassager(ProfilPassager profilPassager){
+        profils.remove(profilPassager);
     }
     
-    public void retirerSourceHeureFin(Metier.Source.SourceHeureFin sourceHeureFin){
-        sources.remove(sourceHeureFin);
-    }
-    
-    public void retirerSourceFinie(Metier.Source.SourceFinie sourceFinie){
-        sources.remove(sourceFinie);
-    }
-    
-    public void retirerProfilPassagerFini(Metier.Profil.ProfilPassagerFini profilPassagerFini){
-        profils.remove(profilPassagerFini);
-    }
-    
-    public void retirerProfilPassagerHeureFin(Metier.Profil.ProfilPassagerHeureFin profilPassagerHeureFin){
-        profils.remove(profilPassagerHeureFin);
-    }
-    
-    public void ajouterSourceHeureFin(double heureFin ,Point pointDepart, double heureDebut, double frequence, DistributionTriangulaire distribution){
-        SourceHeureFin nouveauSourceHeureFin = new SourceHeureFin(heureFin , pointDepart, heureDebut, frequence, distribution);
-        sources.add(nouveauSourceHeureFin);
-    }
-    
-    public void ajouterSourceFinie(int nombreMax ,Point pointDepart, double heureDebut, double frequence, DistributionTriangulaire distribution){
-        SourceFinie nouveauSourceFinie = new SourceFinie(nombreMax , pointDepart, heureDebut, frequence, distribution);
-        sources.add(nouveauSourceFinie);
-    }
-    
-    public void ajouterProfilPassagerFini(int nombreMax, Point point, double heureDepart, double frequence, DistributionTriangulaire distribution){
-        ProfilPassagerFini nouveauProfilPassagerFini = new ProfilPassagerFini(nombreMax, point, heureDepart, frequence, distribution);
-        profils.add(nouveauProfilPassagerFini);
-    }
-    
-    public void ajouterProfilPassagerHeureFin(double heureFin, Point point, double heureDepart, double frequence, DistributionTriangulaire distribution){
-        ProfilPassagerHeureFin nouveauProfilPassagerHeureFin = new ProfilPassagerHeureFin(heureFin, point, heureDepart, frequence, distribution);
-        profils.add(nouveauProfilPassagerHeureFin);
-    }
     
     
     
