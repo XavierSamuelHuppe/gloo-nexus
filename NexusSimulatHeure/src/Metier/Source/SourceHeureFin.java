efc0444867d96e9644395ed918cbf9664ed75604
Pierre Pelletier <pierrepelletier64@gmail.com>
2015-11-13 15:50:43 -0500

    changement distributionTriangulaire pour distribution , retrait de type

--- NexusSimulatHeure/src/Metier/Source/SourceHeureFin.java
+++ NexusSimulatHeure/src/Metier/Source/SourceHeureFin.java
@@ -3,13 +3,13 @@ package Metier.Source;
 
 import Metier.Carte.Point;
 import Metier.Circuit.Circuit;
-import Metier.DistributionTriangulaire;
+import Metier.Distribution;
 
 public class SourceHeureFin extends Source {
     private double heureFin;
     
-    public SourceHeureFin(double heureFin, Point pointDepart, double heureDebut, double frequence, DistributionTriangulaire distribution, String typeSource, int capaciteVehicule, Circuit circuit){
-        super(pointDepart, heureDebut, frequence, distribution, typeSource, capaciteVehicule, circuit);
+    public SourceHeureFin(double heureFin, Point pointDepart, double heureDebut, double frequence, Distribution distribution, int capaciteVehicule, Circuit circuit){
+        super(pointDepart, heureDebut, frequence, distribution, capaciteVehicule, circuit);
         this.heureFin = heureFin;
     }
     
