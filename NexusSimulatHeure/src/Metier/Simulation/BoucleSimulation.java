05cb442cb169179bb39bb4497ee4047ec3437ee2
Bastille <Bastille@Bastille-PC>
2015-11-13 16:13:26 -0500

    la boucle compte maintenant combien de temps la simulation prend pour computer les modifications d'avancement et soustrait ce chiffre à l'attente de la prochaine frame.

--- NexusSimulatHeure/src/Metier/Simulation/BoucleSimulation.java
+++ NexusSimulatHeure/src/Metier/Simulation/BoucleSimulation.java
@@ -12,15 +12,21 @@ public class BoucleSimulation implements Runnable{
     public void run() {
         while(true)
         {
+            int tDelta = 0;
             if(sim.getParametres().estEnAction()){
                 int framerate = sim.getParametres().getFramerate();
                 double ratioVitesse = sim.getParametres().obtenirRatioVitesse();
                 double TempsEcouleParRatioEnSeconde = (1/framerate) * ratioVitesse;
+                
+                //On calcul le temps de computation pour locker le framerate
+                long tStart = System.currentTimeMillis();
                 sim.faireAvancerSimulation(TempsEcouleParRatioEnSeconde);
+                long tEnd = System.currentTimeMillis();
+                tDelta = ((Long)(tEnd - tStart)).intValue();
             }
             
             try {
-                int tempsAAttendreEnMili = 1000/sim.getParametres().getFramerate();
+                int tempsAAttendreEnMili = (1000/sim.getParametres().getFramerate()) - tDelta;
                 Thread.sleep(tempsAAttendreEnMili);
             } catch(InterruptedException ex) {
                 System.out.println("Simulation stopée");
