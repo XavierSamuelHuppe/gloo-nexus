1890b2b0ed27ddac6a97ef352a22736178ff5d7d
XavierSamuelHuppe <xhuppe@gmail.com>
2015-11-19 12:28:15 -0500

    commencerContinuerCircuitEnCours

--- NexusSimulatHeure/src/Metier/ContexteEdition.java
+++ NexusSimulatHeure/src/Metier/ContexteEdition.java
@@ -1,7 +1,12 @@
 package Metier;
 
+import Metier.Carte.Carte;
 import Metier.Carte.Point;
+import Metier.Carte.Segment;
 import Metier.Exceptions.AucunPointActifException;
+import Metier.Exceptions.EditionEnMauvaisModeException;
+import Metier.Simulation.Simulation;
+import java.util.List;
 
 public class ContexteEdition {
 
@@ -15,9 +20,15 @@ public class ContexteEdition {
     
     private ModeEdition mode;
     private Point pointActif;
+    private List<Segment> circuitEnCours;
     
-    public ContexteEdition(){
+    private Simulation simulation;
+    private Carte carte;
+    
+    public ContexteEdition(Simulation simulation, Carte carte){
         mode = ModeEdition.POINT;
+        this.carte = carte;
+        this.simulation = simulation;
     }
     
     public boolean estEnModePoint(){
@@ -61,7 +72,14 @@ public class ContexteEdition {
         return pointActif;
     }
     
-    
+    public void commencerContinuerCreationCircuit(Point p){
+        if(!estEnModeCircuit())
+            throw new EditionEnMauvaisModeException();
+        
+        setPointActif(p);
+        Segment segmentAAjouter = carte.obtenirSegment(pointActif, p);
+        circuitEnCours.add(segmentAAjouter);
+    }
     
     
 }
