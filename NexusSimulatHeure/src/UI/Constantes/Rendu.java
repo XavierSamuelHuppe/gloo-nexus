5d38b01c1c193490dc0999eaf8988580245d12c1
Philippe Plante <plantephilippe@gmail.com>
2015-11-22 15:46:46 -0500

    Plus de retraits d'entêtes

--- NexusSimulatHeure/src/UI/Constantes/Rendu.java
+++ NexusSimulatHeure/src/UI/Constantes/Rendu.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package UI.Constantes;
 
-/**
- *
- * @author The Vagrant Geek
- */
 public class Rendu {
     public static final int TAILLE_TRAIT = 5;
     public static final double FACTEUR_TRAIT_DEUX_TONS_EXTERIEUR = 1.5;
--- NexusSimulatHeure/src/UI/Dessinateurs/DessinateurSegment.java
+++ NexusSimulatHeure/src/UI/Dessinateurs/DessinateurSegment.java
@@ -4,38 +4,25 @@ import java.awt.Graphics2D;
 
 public abstract class DessinateurSegment {
     protected UI.Segment segment;
+    private String tempsAffiche = null;
     
-    
-
-    public DessinateurSegment(UI.Segment s)
-    {
+    public DessinateurSegment(UI.Segment s) {
         segment = s;
     }
     
     public abstract void dessiner(Graphics2D g2);
     public abstract java.awt.Point calculerCentreSegment();
            
-    protected java.awt.Point calculerPositionEntrePoints()
-    {
+    protected java.awt.Point calculerPositionEntrePoints() {
         return new java.awt.Point(segment.getDepart().calculerCentreX() + (int)((segment.getArrivee().calculerCentreX() - segment.getDepart().calculerCentreX()) / 2),
                                   segment.getDepart().calculerCentreY() + (int)((segment.getArrivee().calculerCentreY() - segment.getDepart().calculerCentreY()) / 2));
     }
     
-    private String tempsAffiche = null;
-    protected String obtenirTempsSegmentAffiche()
-    {
-//        if(tempsAffiche == null)
-//        {
-//            tempsAffiche = ((Double)this.segment.getSegmentMetier().getTempsTransit()).toString();
-//        }
-//        return tempsAffiche;
+    protected String obtenirTempsSegmentAffiche() {
         if(this.segment.getSegmentMetier().getTempsTransit() > 0.0)
         {
             return ((Double)this.segment.getSegmentMetier().getTempsTransit()).toString();
         }
         return "";
-        
     }
-    
-
 }
