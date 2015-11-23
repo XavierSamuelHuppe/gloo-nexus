a29a246ee89d857b2f37d9b98bbd238786c3983a
Bastille <Bastille@Bastille-PC>
2015-11-23 14:08:08 -0500

    règlé le problème des sourceHeureFin qui ne spawnait pas de vehicule

--- NexusSimulatHeure/src/Metier/Source/SourceHeureFin.java
+++ NexusSimulatHeure/src/Metier/Source/SourceHeureFin.java
@@ -5,19 +5,21 @@ import Metier.Carte.Point;
 import Metier.Circuit.Circuit;
 import Metier.Distribution;
 import Metier.Circuit.ConteneurPassagers;
+import Metier.Simulation.ParametreSimulation;
 import Metier.Simulation.Simulation;
+import java.time.DateTimeException;
 import java.time.LocalTime;
 
 public class SourceHeureFin extends Source {
     private LocalTime heureFin;
     
     private LocalTime prochaineGeneration;
-    private int nombreCree = 0;
+    private int nombreCree;
     
     public SourceHeureFin(LocalTime heureFin, Point pointDepart, LocalTime heureDebut, Distribution distribution, ConteneurPassagers passagers, Circuit circuit, Simulation sim){
         super(pointDepart, heureDebut, distribution, passagers, circuit, sim);
         this.heureFin = heureFin;
-        
+        nombreCree = 0;
         prochaineGeneration = heureDebut;
     }
     
@@ -31,14 +33,29 @@ public class SourceHeureFin extends Source {
     
     @Override
     public void avancerCreation(LocalTime heureCourante, double tempsEcouleParRatioEnSeconde) {
-        if(prochaineGeneration.isAfter(heureFin))
+        if(!doitSpawnerVehicule(heureCourante))
             return;
         
         if(prochaineGeneration.isBefore(heureCourante)){
             genererVehicule();
             nombreCree++;
-            prochaineGeneration = heureDebut.plusSeconds((long)(getFrequence() * (double)nombreCree));
+            prochaineGeneration = prochaineGeneration.plusSeconds((long)/*(*/getFrequence()/* * (double)nombreCree)*/);
         }
+    }
+    
+    private boolean doitSpawnerVehicule(LocalTime heureCourante){
+        LocalTime heureDebutNouvelleJournee = ParametreSimulation.HEURE_DEBUT_NOUVELLE_JOURNEE;
+        LocalTime minuit = LocalTime.MIDNIGHT;
+        LocalTime justeAvantMinuit = LocalTime.MAX;
+        if(heureFin.isBefore(justeAvantMinuit) && heureFin.isAfter(heureDebutNouvelleJournee)){
+            return heureCourante.isBefore(heureFin);
+        }else if(heureFin.isAfter(minuit) && heureFin.isBefore(heureDebutNouvelleJournee)){
+            if(heureCourante.isBefore(justeAvantMinuit) && heureCourante.isAfter(heureDebutNouvelleJournee))
+                return true;
+            else
+                return heureCourante.isBefore(heureFin);
+        }
+        throw new DateTimeException("Les paramètres de la simulation ne sont pas bien réglés.");
     }
     
     @Override
