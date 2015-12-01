ebe195d2ff0b52c04d4c45fd8c91223d0ac8cbbd
Pierre Pelletier <pierrepelletier64@gmail.com>
2015-12-01 00:44:07 -0500

    modification du constructeur de ProfilPassager (changer les double en local time , ajout de Simulation)

--- NexusSimulatHeure/src/Metier/Profil/ProfilPassager.java
+++ NexusSimulatHeure/src/Metier/Profil/ProfilPassager.java
@@ -3,25 +3,31 @@ package Metier.Profil;
 
 import Metier.Carte.Point;
 import Metier.Distribution;
+import Metier.Simulation.Simulation;
 import Metier.Simulation.Statistiques;
+import java.time.LocalTime;
 
 public abstract class ProfilPassager {
     private Point pointDepart;
-    private double heureDepart;
+    private LocalTime heureDepart;
     private double frequence;
     public Distribution distributionAUtiliser;
     public Statistiques statistiques;
     
-    public ProfilPassager(Point point, double heureDepart, double frequence, Distribution distribution){
-        this.frequence = frequence;
+    public ProfilPassager(Point point, LocalTime heureDepart, Distribution distribution, Simulation simulation){
         this.heureDepart = heureDepart;
         this.pointDepart = point;
         this.distributionAUtiliser = distribution;
+        
     }
     public Point getPointDepart(){
         return pointDepart;
     }
-    public double getHeureDepart(){
+    public Distribution getDistribution()
+    {
+        return distributionAUtiliser;
+    }
+    public LocalTime getHeureDepart(){
         return heureDepart;
     }
     public double getFrequence(){
@@ -30,24 +36,42 @@ public abstract class ProfilPassager {
     public void setPointDepart(Point point){
         pointDepart = point;
     }
-    public void setHeureDepart(int heureD){
+    public void setHeureDepart(LocalTime heureD){
         heureDepart = heureD;
     }
     public void setFrequence(int freq){
         frequence = freq;
     }
+    public void setDistribution(Distribution d) {
+        this.distributionAUtiliser = d;
+    }
+    
     
     
     public void pigerDonneesDepart()
     {
-        //
+        frequence = distributionAUtiliser.obtenirProchaineValeurAleatoire();
     }
+    /*
+    public void rafraichirDonneesDepart(){
+        frequence = 0;
+        reInitialiserValeursDepartSimulation();
+    }*/
     
-    /*   
+    
+    
+    /*
     public Passager genererPassager()
     {
         Passager unPieton = new Passager();
+        
         return unPieton;
-    }
-    */
+    }*/
+    
+    /*
+    @Override
+    public String toString()
+    {
+        return this.obtenirDescriptionProfil();
+    }*/
 }
