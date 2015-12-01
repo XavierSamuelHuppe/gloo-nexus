3311415b45f1bf7b0d00183a934a9775c21eece4
Pierre Pelletier <pierrepelletier64@gmail.com>
2015-12-01 00:44:52 -0500

    modification du constructeur des héritier de ProfilPassager (changer les double en local time , ajout de Simulation)

--- NexusSimulatHeure/src/Metier/Profil/ProfilPassagerHeureFin.java
+++ NexusSimulatHeure/src/Metier/Profil/ProfilPassagerHeureFin.java
@@ -2,20 +2,22 @@ package Metier.Profil;
 
 import Metier.Carte.Point;
 import Metier.Distribution;
+import Metier.Simulation.Simulation;
+import java.time.LocalTime;
 
 public class ProfilPassagerHeureFin extends ProfilPassager {
     
-    private double heureFin;
+    private LocalTime heureFin;
     
-    public ProfilPassagerHeureFin(double heureFin, Point point, double heureDepart, double frequence, Distribution distribution){
-        super(point, heureDepart, frequence, distribution);
+    public ProfilPassagerHeureFin(LocalTime heureFin, Point point, LocalTime heureDepart, Distribution distribution, Simulation simulation){
+        super(point, heureDepart, distribution, simulation);
         this.heureFin = heureFin;
     }
     
-    public double getHeureFin(){
+    public LocalTime getHeureFin(){
         return heureFin;
     }
-    public void setHeureFin(int Fin){
+    public void setHeureFin(LocalTime Fin){
         heureFin = Fin;
     }
 }
