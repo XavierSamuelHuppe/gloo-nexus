18b123d05c6da05a004bc8b8c1073ec59db71b28
Pierre Pelletier <pierrepelletier64@gmail.com>
2015-12-01 00:42:44 -0500

    Creation de ProfilBuilder afin de pouvoir créer des ProfilPassager

---
+++ NexusSimulatHeure/src/Metier/Profil/ProfilBuilder.java
@@ -0,0 +1,25 @@
+
+package Metier.Profil;
+
+import Metier.Carte.Point;
+import Metier.Circuit.Circuit;
+import Metier.Circuit.ConteneurPassagers;
+import Metier.Distribution;
+import Metier.Simulation.Simulation;
+import Metier.Profil.ProfilPassager;
+import Metier.Profil.ProfilPassagerFini;
+import Metier.Profil.ProfilPassagerHeureFin;
+import java.time.LocalTime;
+
+
+public class ProfilBuilder {
+    public ProfilPassager ConstruireProfil(int nombreMax, Point pointDepart, LocalTime heureDebut, Distribution distribution, Simulation sim){
+        ProfilPassager nouveauProfil = new ProfilPassagerFini(nombreMax , pointDepart, heureDebut, distribution, sim);
+        return nouveauProfil;
+    }
+    
+    public ProfilPassager ConstruireProfil(LocalTime heureFin, Point pointDepart, LocalTime heureDebut, Distribution distribution, Simulation sim){
+        ProfilPassager nouveauProfil = new ProfilPassagerHeureFin(heureFin , pointDepart, heureDebut ,  distribution, sim);
+        return nouveauProfil;
+    }
+}
