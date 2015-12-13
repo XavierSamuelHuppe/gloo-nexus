2a0b4ede0d6bd9d0a907ca236cd9c264a0931d97
Pierre Pelletier <pierrepelletier64@gmail.com>
2015-12-13 15:40:43 -0500

    Changement léger du regex pour obliger le format d'heure 00:00:00  au lieu de 0:00:00 . causait un plantage

--- NexusSimulatHeure/src/UI/Constantes/Validations.java
+++ NexusSimulatHeure/src/UI/Constantes/Validations.java
@@ -1,5 +1,5 @@
 package UI.Constantes;
 
 public class Validations {
-    public static final String REGEX_FORMAT_HEURE = "^(([0-1]?[0-9])|([2][0-3])):([0-5]?[0-9]):([0-5]?[0-9])$";
+    public static final String REGEX_FORMAT_HEURE = "^(([0-1]?[0-9])&([2][0-3])):([0-5]?[0-9]):([0-5]?[0-9])$";
 }
